da2aae5f2cf3cfe9b8f3d0f0f15e88ef23e401d8
Sam Glenn <samglennss3@gmail.com>
2023-07-23 02:42:51 +0200

    getting most of the logic

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 import { CoinGeckoClient, SimplePriceResponse } from "coingecko-api-v3";
 import TelegramBot from "node-telegram-bot-api";
 import dotenv from "dotenv";
-import { ethers } from "ethers";
+import { BigNumber, ethers } from "ethers";
 import {
   ETH_USDC_POOL_ADDRESS,
   KYBER_ROUTER_ADDRESS,
@@ -52,34 +52,67 @@ async function init() {
     signer
   );
 
-  // estimate gas price
-  const gasPrice = await zksyncProvider.getGasPrice();
-  const currentEthBlock = await ethProvider.getBlock("latest");
-  const timestamp = currentEthBlock.timestamp;
-  const gasLimitEstimate = await contract.estimateGas.swapExactTokensForETH(
-    0,
-    0,
-    [ETH_USDC_POOL_ADDRESS],
-    [USDC_ADDRESS, WETH_ADDRESS],
-    "0x05bfb506cbd63bb468c903d53dfef1c72f47d974", //TODO: Needs to be the address of the user
-    timestamp + 1000
-  );
-
-  // the total gas cost is gasPrice * gasLimit
-  const totalGasCost = gasPrice.mul(gasLimitEstimate);
-
-  console.log("gasLimitEstimate", gasLimitEstimate.toString());
-  console.log("gasPrice", gasPrice.toString());
-  console.log("totalGasCost", totalGasCost.toString());
-
   const bot = new TelegramBot(token as string, { polling: true });
 
-  bot.onText(/\/echo (.+)/, (msg: any, match: any) => {
+  async function executeAction() {
+    // estimate gas price
+    const gasPrice = await zksyncProvider.getGasPrice();
+    const currentEthBlock = await ethProvider.getBlock("latest");
+    const timestamp = currentEthBlock.timestamp;
+    const gasLimitEstimate = await contract.estimateGas.swapExactTokensForETH(
+      100 * 10 ** 6, //6 decimals: 100 USDC
+      1,
+      [ETH_USDC_POOL_ADDRESS],
+      [USDC_ADDRESS, WETH_ADDRESS],
+      "0x05bfb506cbd63bb468c903d53dfef1c72f47d974", //TODO: Needs to be the address of the user
+      timestamp + 1000
+    );
+    // the total gas cost is gasPrice * gasLimit
+    const totalGasCost = gasPrice.mul(gasLimitEstimate);
+
+    console.log("gasLimitEstimate", gasLimitEstimate.toString());
+    console.log("gasPrice", gasPrice.toString());
+    console.log("totalGasCost", totalGasCost.toString());
+
+    const totalGasCostWithBuffer = totalGasCost.mul(3);
+
+    // get amounts in
+    const usdcAmountForEth = await contract.getAmountsIn(
+      totalGasCostWithBuffer,
+      [ETH_USDC_POOL_ADDRESS],
+      [USDC_ADDRESS, WETH_ADDRESS]
+    );
+    console.log("usdcAmountForEth", usdcAmountForEth);
+
+    const amountIn = 100 * 10 ** 6 - usdcAmountForEth[0]; // TODO: check if this is correct
+
+    const ethAmountForUsdc = await contract.getAmountsOut(
+      amountIn,
+      [ETH_USDC_POOL_ADDRESS],
+      [USDC_ADDRESS, WETH_ADDRESS]
+    );
+    const amountOut = BigNumber.from(ethAmountForUsdc).mul(70).div(100);
+
+    // TODO: Uncomment eventually
+    // const result = await contract.swapExactTokensForETH(
+    //   amountIn,
+    //   amountOut,
+    //   [ETH_USDC_POOL_ADDRESS],
+    //   [USDC_ADDRESS, WETH_ADDRESS],
+    //   "0x05bfb506cbd63bb468c903d53dfef1c72f47d974", //TODO: Needs to be the address of the user
+    //   timestamp + 1000
+    // );
+    // console.log(result);
+    // bot.sendMessage(chatId, result);
+  }
+
+  bot.onText(/\/echo (.+)/, async (msg: any, match: any) => {
     const chatId = msg.chat.id;
     const resp = match[1];
 
     if (chatId == groupID) {
-      bot.sendMessage(chatId, resp);
+      // test by sending /echo something
+      await executeAction();
     }
   });
 
@@ -91,9 +124,10 @@ async function init() {
         vs_currencies: "usd",
       });
 
-      console.log(data["usd-coin"].usd);
-
-      // calculate the gas cost times 3 x
+      if (data["usd-coin"].usd <= 0.85) {
+        bot.sendMessage(groupID as string, "USDC is below 0.85");
+        await executeAction();
+      }
 
       // delay for 20 seconds
       await new Promise((resolve) => setTimeout(resolve, 20000));
